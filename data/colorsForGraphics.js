eb2be0060afb51b94c937234088bc2f1585829b3
Shirley Silvana Suarez Startary <shirley.startary@gmail.com>
2020-05-28 20:33:38 -0500

    Add card to graphic and refactoring the information graphics

---
+++ data/colorsForGraphics.js
@@ -0,0 +1,89 @@
+const objetivos = [
+  {
+    label: 'objetivo 1 ',
+    value: '15',
+    color: '#eb1c2d',
+  },
+  {
+    label: 'objetivo 2 ',
+    value: '11',
+    color: '#D3A029',
+  },
+  {
+    label: 'objetivo 3 ',
+    value: '5',
+    color: '#279B48',
+  },
+  {
+    label: 'objetivo 4 ',
+    value: '2',
+    color: '#C31F33',
+  },
+  {
+    label: 'objetivo 5 ',
+    value: '7',
+    color: '#EF402B',
+  },
+  {
+    label: 'objetivo 6 ',
+    value: '5',
+    color: '#26AED9',
+  },
+  {
+    label: 'objetivo 7 ',
+    value: '1',
+    color: '#FCB712',
+  },
+  {
+    label: 'objetivo 8 ',
+    value: '1',
+    color: '#8F1838',
+  },
+  {
+    label: 'objetivo 9 ',
+    value: '2',
+    color: '#F36D26',
+  },
+  {
+    label: 'objetivo 10',
+    value: '9',
+    color: '#E11484',
+  },
+  {
+    label: 'objetivo 11',
+    value: '7',
+    color: '#F99D26',
+  },
+  {
+    label: 'objetivo 12',
+    value: '5',
+    color: '#CF8D2A',
+  },
+  {
+    label: 'objetivo 13',
+    value: '5',
+    color: '#48773D',
+  },
+  {
+    label: 'objetivo 14',
+    value: '5',
+    color: '#187DBC',
+  },
+  {
+    label: 'objetivo 15',
+    value: '5',
+    color: '#3EB049',
+  },
+  {
+    label: 'objetivo 16',
+    value: '13',
+    color: '#0D568B',
+  },
+  {
+    label: 'objetivo 17',
+    value: '5',
+    color: '#183668',
+  },
+];
+
+export default objetivos;
--- pages/graphics.jsx
+++ pages/graphics.jsx
@@ -1,97 +1,12 @@
 /* eslint-disable no-console */
 import React from 'react';
 import Chart from 'chart.js';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import countrys from '../map-world';
 import NavigationBar from '../components/navigationBar';
-
-const objetivos = [
-  {
-    label: 'objetivo 1 ',
-    value: '15',
-    color: '#eb1c2d',
-  },
-  {
-    label: 'objetivo 2 ',
-    value: '11',
-    color: '#D3A029',
-  },
-  {
-    label: 'objetivo 3 ',
-    value: '5',
-    color: '#279B48',
-  },
-  {
-    label: 'objetivo 4 ',
-    value: '2',
-    color: '#C31F33',
-  },
-  {
-    label: 'objetivo 5 ',
-    value: '7',
-    color: '#EF402B',
-  },
-  {
-    label: 'objetivo 6 ',
-    value: '5',
-    color: '#26AED9',
-  },
-  {
-    label: 'objetivo 7 ',
-    value: '1',
-    color: '#FCB712',
-  },
-  {
-    label: 'objetivo 8 ',
-    value: '1',
-    color: '#8F1838',
-  },
-  {
-    label: 'objetivo 9 ',
-    value: '2',
-    color: '#F36D26',
-  },
-  {
-    label: 'objetivo 10',
-    value: '9',
-    color: '#E11484',
-  },
-  {
-    label: 'objetivo 11',
-    value: '7',
-    color: '#F99D26',
-  },
-  {
-    label: 'objetivo 12',
-    value: '5',
-    color: '#CF8D2A',
-  },
-  {
-    label: 'objetivo 13',
-    value: '5',
-    color: '#48773D',
-  },
-  {
-    label: 'objetivo 14',
-    value: '5',
-    color: '#187DBC',
-  },
-  {
-    label: 'objetivo 15',
-    value: '5',
-    color: '#3EB049',
-  },
-  {
-    label: 'objetivo 16',
-    value: '13',
-    color: '#0D568B',
-  },
-  {
-    label: 'objetivo 17',
-    value: '5',
-    color: '#183668',
-  },
-];
+import objetivos from '../data/colorsForGraphics';
 
 class BarChart extends React.Component {
   constructor(props) {
@@ -211,29 +126,42 @@ class Graphics extends React.Component {
     return (
       <div>
         <NavigationBar />
-        <div className="main">
-          <div className="chart-wrapper">
-            <DoughnutChart
-              data={data}
-              title="Objetivo más seleccionado"
-              color=""
-            />
-          </div>
-          <div className="chart-wrapper">
-            <BarChart
-              data={data}
-              title="Objetivo mejor respondido"
-              color=""
-            />
-          </div>
-          <div className="chart-wrapper">
-            <Typography gutterBottom align="center">
-              Países con interacción
-            </Typography>
-            <Map />
-          </div>
+        <div className="position">
+            {/* <div className="main"> */}
+          <Card elevation={4}>
+            <CardContent>
+              <div className="chart-wrapper">
+                <DoughnutChart
+                  data={data}
+                  title="Objetivo más seleccionado"
+                  color=""
+                />
+              </div>
+            </CardContent>
+          </Card>
+          <Card elevation={4}>
+            <CardContent>
+              <div className="chart-wrapper">
+                <BarChart
+                  data={data}
+                  title="Objetivo mejor respondido"
+                  color=""
+                />
+              </div>
+            </CardContent>
+          </Card>
+          <Card elevation={4}>
+            <CardContent>
+              <div className="chart-wrapper">
+                <Typography gutterBottom align="center">
+                  Países con interacción
+                </Typography>
+                <Map />
+              </div>
+            </CardContent>
+          </Card>
+        {/* </div> */}
         </div>
-
         <style jsx>
           {`
             .main {
@@ -253,6 +181,11 @@ class Graphics extends React.Component {
               margin: 0;
               font-family: system-ui;
               background: #F7F7F7;
+            }
+            .position {
+              display: grid;
+              grid-gap: 30px;
+              padding: 10%;
             }
           `}
         </style>
